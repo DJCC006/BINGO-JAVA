7ef70f065c2fe6533261840a80c388a1625e7867
DJCC <david.carcamo2006@gmail.com>
2025-09-08 08:48:08 -0600

    Creacion inicial de proyecto

---
+++ JAVA_BINGO/src/main/java/com/mycompany/java_bingo/JAVA_BINGO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_bingo;
+
+/**
+ *
+ * @author David
+ */
+public class JAVA_BINGO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
